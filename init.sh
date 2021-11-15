499adfae1b0b6909751d277dcdb9e0f9ed3b4d35
PranayAnchuri <38789227+PranayAnchuri@users.noreply.github.com>
2021-11-15 15:15:57 -0500

    remove installation script

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-conda create --name pds python=3.8
-conda activate pds
-pip install pandas==1.3.0
-pip install auto-sklearn
-pip install -U "mxnet<2.0.0"
-pip install --pre autogluon
